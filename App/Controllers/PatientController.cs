940531b77ae32f52262055b321af45c0e0676c1b
Ankon190 <ankon190@gmail.com>
2026-05-15 00:06:32 +0600

    patient Controller created

---
+++ App/Controllers/PatientController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BLL.Services;
+using BLL.DTOs;
+
+namespace App.Controllers
+{
+    public class PatientController : Controller
+    {
+        PatientService patientService;
+        public PatientController(PatientService patientService)
+        {
+            this.patientService = patientService;
+        }
+        public IActionResult Index()
+        {
+            var data = patientService.Get();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(PatientDTO p)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = patientService.Create(p);
+                if(res == true)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(p);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var data = patientService.Get(id);
+            return View(data);
+        }
+        [HttpPost]
+        public IActionResult Edit(PatientDTO p)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = patientService.Update(p);
+                if (res == true)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(p);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var data = patientService.Get(id);
+            return View(data);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id, string Decision)
+        {
+            if (Decision.Equals("Yes"))
+            {
+                patientService.Delete(id);
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
